200ea7d8d962b1568aea4fa5e38ae91a894c2920
wzrdvxyd <cjesusmunoz@outlook.es>
2025-10-19 17:37:02 -0300

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "tienda";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa";
+}
+?>
